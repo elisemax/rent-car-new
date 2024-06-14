718644abd7d4f5d73290d2cf378e0892ac461118
Maxim ELISEYKIN <maxim.eliseykin@tipsport.cz>
2024-06-14 14:16:37 +0200

    fix middle

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ["en", "cs", "ru"],
+  locales: ["cs", "en", "ru"],
 
   // Used when no locale matches
   defaultLocale: "cs",
